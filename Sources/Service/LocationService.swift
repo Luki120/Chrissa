a55240021b428a51434c98ef4749f5652c266cc2
luki120 <luki120@exerra.xyz>
2025-06-16 05:28:15 -0300

    initial support for jailed (tweaks, apps [installed as deb / sideloaded])

--- Sources/Service/LocationService.swift
+++ Sources/Service/LocationService.swift
@@ -1,17 +1,31 @@
 import CoreLocation
 
-/// Manager class to handle fetching the user's location
-internal final class LocationService: ObservableObject {
-
+/// Singleton class to handle fetching the user's location
+internal
+final class LocationService: NSObject, ObservableObject {
 	private let geoCoder = CLGeocoder()
 	private let manager = CLLocationManager.shared()
+
+	private var isSpringBoard: Bool {
+		return Bundle.main.bundleIdentifier == "com.apple.springboard"
+	}
+
+	private lazy var locationManager: CLLocationManager = {
+		let manager = CLLocationManager()
+		manager.desiredAccuracy = kCLLocationAccuracyBest
+		manager.allowsBackgroundLocationUpdates = true
+		manager.pausesLocationUpdatesAutomatically = false
+		return manager
+	}()
 
 	@Published private(set) internal var latitude: CLLocationDegrees = 0
 	@Published private(set) internal var longitude: CLLocationDegrees = 0
 	@Published private(set) internal var locationName = ""
 
-	internal init() {
-		forceEnableLocation()
+	internal
+	override init() {
+		super.init()
+		isSpringBoard ? forceEnableLocation() : fetchLocation()
 	}
 
 	internal func forceEnableLocation() {
@@ -25,11 +39,35 @@ internal final class LocationService: ObservableObject {
 		longitude = location.coordinate.longitude
 
 		manager.stopUpdatingLocation()
+		fetchLocationName(for: location)
+	}
 
+	// ! Private
+
+	private func fetchLocation() {
+		locationManager.delegate = self
+		locationManager.requestAlwaysAuthorization()
+		locationManager.startUpdatingLocation()
+	}
+
+	private func fetchLocationName(for location: CLLocation) {
 		geoCoder.reverseGeocodeLocation(location) { placemarks, error in
 			guard error == nil, let placemarks, let placemark = placemarks.first else { return }
 			self.locationName = placemark.locality ?? "N/A"
 		}
 	}
+}
 
+// ! CLLocationManagerDelegate
+
+extension LocationService: CLLocationManagerDelegate {
+	func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {}
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		guard let location = locations.last else { return }
+		latitude = location.coordinate.latitude
+		longitude = location.coordinate.longitude
+
+		locationManager.stopUpdatingLocation()
+		fetchLocationName(for: location)
+	}
 }
--- Sources/Service/WeatherService.swift
+++ Sources/Service/WeatherService.swift
@@ -1,10 +1,9 @@
 import Combine
 import UIKit
 
-/// Weather manager class to handle API calls
+/// Singleton class to make weather API calls
 @objc(CHWeatherService)
 public final class WeatherService: NSObject, ObservableObject {
-
 	/// Shared instance
 	@objc(sharedInstance)
 	public static let shared = WeatherService()
@@ -17,52 +16,89 @@ public final class WeatherService: NSObject, ObservableObject {
 		static let baseURL = "https://api.open-meteo.com/v1/forecast?"
 	}
 
-	/// String that represents the current location name
-	@Published private(set) public var locationName = ""
+	/// `String` that represents the current location name
+	@Published
+	private(set) public var locationName = ""
 
-	private override init() {
+	private
+	override init() {
 		super.init()
 	}
 
-}
+	// ! Reusable
 
-extension WeatherService {
-
-	// ! Public
-
-	/// Combine function to make API calls
-	/// - Parameters:
-	///		- expecting: The type that conforms to Codable
-	/// - Returns: AnyPublisher of generic type T & Error
-	/// - Throws: An error of type URLError
-	public func fetchWeather<T: Codable>(expecting type: T.Type = Weather.self) throws -> AnyPublisher<T, Error> {
-		locationService.forceEnableLocation()
+	private func weatherPublisher<T: Codable>(
+		latitude: CLLocationDegrees,
+		longitude: CLLocationDegrees,
+		expecting type: T.Type
+	) -> AnyPublisher<T, Error> {
 		locationService.$locationName
 			.sink { [weak self] name in
 				self?.locationName = name
 			}
 			.store(in: &subscriptions)
 
-		let apiURL = "\(Constants.baseURL)latitude=\(locationService.latitude)&longitude=\(locationService.longitude)&current=temperature_2m,apparent_temperature,weather_code,is_day&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&forecast_days=1&timeformat=unixtime&timezone=auto"
-		guard let url = URL(string: apiURL) else { throw URLError(.badURL) }
+		let apiURL = "\(Constants.baseURL)latitude=\(latitude)&longitude=\(longitude)&current=temperature_2m,apparent_temperature,weather_code,is_day&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&forecast_days=1&timeformat=unixtime&timezone=auto"
+
+		guard let url = URL(string: apiURL) else {
+			return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+		}
 
 		return URLSession.shared.dataTaskPublisher(for: url)
 			.tryMap { data, response in
 				guard let response = response as? HTTPURLResponse,
 					(200..<300).contains(response.statusCode) else {
-						throw(URLError(.badServerResponse))
+						throw URLError(.badServerResponse)
 					}
 				return data
 			}
-			.decode(type: type.self, decoder: JSONDecoder())
+			.decode(type: type, decoder: JSONDecoder())
 			.receive(on: DispatchQueue.main)
 			.eraseToAnyPublisher()
 	}
+}
 
-	/// Function to make API calls
-	/// - Parameters:
-	///		- completion: Escaping closure that takes a Weather object & a String as argument & returns nothing
-	/// - Throws: An error of type URLError
+// ! Public
+
+extension WeatherService {
+	/// Function to fetch weather data, leveraging Combine pipelines
+	/// - Parameter expecting: The type that conforms to `Codable`, defaults to `Weather`
+	/// - Returns: `AnyPublisher<T, Error>`
+	/// - Throws: `URLError`
+	public func fetchWeather<T: Codable>(expecting type: T.Type = Weather.self) throws -> AnyPublisher<T, Error> {
+		locationService.forceEnableLocation()
+		return weatherPublisher(
+			latitude: locationService.latitude,
+			longitude: locationService.longitude,
+			expecting: type
+		)
+	}
+
+	/// Function to fetch weather data after getting valid location coordinates
+	/// - Returns: `AnyPublisher<Weather, Error>`
+	/// - Throws: `URLError`
+	public func fetchWeather() throws -> AnyPublisher<Weather, Error> {
+		return locationService.$latitude
+			.combineLatest(locationService.$longitude)
+			.filter { latitude, longitude in
+				return latitude != 0 && longitude != 0
+			}
+			.flatMap { [weak self] latitude, longitude -> AnyPublisher<Weather, Error> in
+				guard let self else {
+					return Fail(error: URLError(.unknown)).eraseToAnyPublisher()
+				}
+				return self.weatherPublisher(
+					latitude: latitude,
+					longitude: longitude,
+					expecting: Weather.self
+				)
+			}
+			.eraseToAnyPublisher()
+	}
+
+	/// Function to fetch weather data
+	/// - Parameter completion: `@escaping` closure that takes a `Weather` object & a `String` as argument & returns nothing
+	/// - Throws: `URLError`
 	@objc
 	public func fetchWeather(completion: @escaping (Weather, String) -> Void) throws {
 		fetchWeatherSubscription = nil
@@ -76,5 +112,4 @@ extension WeatherService {
 				completion(weather, locationName)
 			}
 	}
-
 }
